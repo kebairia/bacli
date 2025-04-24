fad0e10467d0f93671810b3df2f1432bfdead378
Zakaria Kebairia <zakaria.kebairia@emploitic.com>
2025-04-24 23:28:56 +0000

    clean(main): cleanup for the main function

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "github.com/kebairia/backup/internal/operations"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/kebairia/backup/internal/operations"
+)
 
 func main() {
-	if err := operations.PerformAllBackups(); err != nil {
-		panic(err)
+	configPath := flag.String("c", "./configs/config.yaml", "Path to YAML config file")
+	doBackup := flag.Bool("B", false, "Backup all databases")
+	// doRestore := flag.Bool("R", false, "Restore all databases")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if *doBackup {
+		if err := operations.BackupAll(*configPath); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			os.Exit(1)
+		}
 	}
 }
