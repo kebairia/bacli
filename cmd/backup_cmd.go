e3bedbd6cdc5b71ea48e41519088f97e8550f48a
Zakaria Kebairia <zakaria.kebairia@emploitic.com>
2025-04-25 11:37:39 +0000

    add(cmd): Add CLI using cobra, first code

---
+++ cmd/backup_cmd.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kebairia/backup/internal/operations"
+	"github.com/spf13/cobra"
+)
+
+var backupCmd = &cobra.Command{
+	Use:   "backup",
+	Short: "Backup all databases as per config",
+	Run: func(cmd *cobra.Command, args []string) {
+		if ConfigFile == "" {
+			fmt.Errorf("Config file is required (-f flag)")
+		}
+		fmt.Println("Starting backup run...")
+		if err := operations.BackupAll(ConfigFile); err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("All backups completed.")
+	},
+}
+
+func init() {
+	// Bind the config file flag to the global variable.
+	backupCmd.Flags().
+		StringVarP(&ConfigFile, "config", "c", "./configs/config.yaml", "path to YAML config file")
+}
---
+++ cmd/root.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/kebairia/backup/internal/logger"
+	"github.com/spf13/cobra"
+)
+
+// ConfigFile is the path to the YAML configuration.
+var (
+	ConfigFile string
+	// rootCmd is the base command for bacli.
+	rootCmd = &cobra.Command{
+		Use:   "bacli",
+		Short: "CLI tool for database backup and restore",
+		Long: `bacli provides subcommands to back up and restore
+databases based on your YAML configuration file.`,
+	}
+)
+
+// Execute runs the root command.
+func Execute() {
+	log, err := logger.Init()
+	_ = err
+	if err := rootCmd.Execute(); err != nil {
+		log.Error("Error executing command: %v", err)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(backupCmd)
+}
