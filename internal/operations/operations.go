47e83f834b9a5059d2487c41c205cb2459dc9b1e
Zakaria Kebairia <4.kebairia@gmail.com>
2025-05-17 00:28:52 +0100

    refactor(operations): changes following the new vault/database package's code

--- internal/operations/operations.go
+++ internal/operations/operations.go
@@ -1,33 +1,43 @@
 package operations
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
 	"time"
 
-	"github.com/kebairia/backup/internal/backup"
 	"github.com/kebairia/backup/internal/config"
+	"github.com/kebairia/backup/internal/database"
 	"github.com/kebairia/backup/internal/logger"
 	"github.com/kebairia/backup/internal/vault"
 )
 
 // OperationManager is a struct that manages the backup and restore operations.
 type OperationManager struct {
+	ctx         context.Context
 	cfg         config.Config
 	vaultClient *vault.Client
 	log         logger.Logger
 }
 
+// NOTE: I don't see any use of the context here, check it
+
 // NewOperationManager loads, parses, and validates the YAML config at configPath.
 func NewOperationManager(configPath string) (*OperationManager, error) {
 	var config config.Config
+	ctx := context.Background()
 	if err := config.Load(configPath); err != nil {
 		return nil, err
 	}
+	//  Build Vault options
+	vaultOpts := []vault.Option{
+		vault.WithAddress(config.Vault.Address),
+		vault.WithAppRole(config.Vault.RoleID, config.Vault.RoleName),
+	}
 	// Init Vault client
-	vaultClient, err := vault.NewClient(config.Vault.Address, config.Vault.Token)
+	vaultClient, err := vault.NewClient(ctx, vaultOpts...)
 	if err != nil {
 		return nil, fmt.Errorf("vault client init: %w", err)
 	}
@@ -35,6 +45,7 @@ func NewOperationManager(configPath string) (*OperationManager, error) {
 	log := logger.Global()
 
 	return &OperationManager{
+		ctx:         ctx,
 		cfg:         config,
 		vaultClient: vaultClient,
 		log:         log,
@@ -42,27 +53,26 @@ func NewOperationManager(configPath string) (*OperationManager, error) {
 }
 
 // InitializeMongoDBInstance loads, parses, and validates the YAML config at configPath.
-func (om *OperationManager) InitializeMongoDBInstance() ([]backup.Database, error) {
-	var dbs []backup.Database
+func (om *OperationManager) InitializeMongoDBInstance() ([]database.Database, error) {
+	var dbs []database.Database
 	for _, instance := range om.cfg.MongoDB.Instances {
-		secretPath := filepath.Join(om.cfg.MongoDB.Vault.KVBase, instance.KVPath)
-		rolePath := filepath.Join(om.cfg.MongoDB.Vault.RoleBase, instance.RoleName)
-		connection, err := om.vaultClient.FullDBConnection(secretPath, rolePath)
+		rolePath := filepath.Join(om.cfg.Postgres.Vault.RoleBase, instance.RoleName)
+		secrets, err := om.vaultClient.GetDynamicCredentials(om.ctx, rolePath)
 		if err != nil {
 			return nil, fmt.Errorf("vault read : %w", err)
 		}
-		opts := []backup.MongoDBOption{
-			backup.WithMongoCredentials(connection.Username, connection.Password),
-			backup.WithMongoDatabase(connection.Database),
-			backup.WithMongoHost(connection.Host),
-			backup.WithMongoPort(connection.Port),
-			backup.WithMongoMethod(instance.Method),
+		opts := []database.MongoDBOption{
+			database.WithMongoCredentials(secrets.Username, secrets.Password),
+			database.WithMongoDatabase(instance.Database),
+			database.WithMongoHost(instance.Host),
+			database.WithMongoPort(instance.Port),
+			database.WithMongoMethod(instance.Method),
 		}
-		mg, err := backup.NewMongoDB(om.cfg, opts...)
+		mg, err := database.NewMongoDB(om.cfg, opts...)
 		if err != nil {
 			return nil, fmt.Errorf(
 				"failed creating MongoDB instance %q: %w",
-				connection.Database,
+				instance.Database,
 				err,
 			)
 		}
@@ -72,27 +82,26 @@ func (om *OperationManager) InitializeMongoDBInstance() ([]backup.Database, erro
 }
 
 // InitializePostgresInstance loads, parses, and validates the YAML config at configPath.
-func (om *OperationManager) InitializePostgresInstance() ([]backup.Database, error) {
-	var dbs []backup.Database
+func (om *OperationManager) InitializePostgresInstance() ([]database.Database, error) {
+	var dbs []database.Database
 	for _, instance := range om.cfg.Postgres.Instances {
-		secretPath := filepath.Join(om.cfg.Postgres.Vault.KVBase, instance.KVPath)
 		rolePath := filepath.Join(om.cfg.Postgres.Vault.RoleBase, instance.RoleName)
-		connection, err := om.vaultClient.FullDBConnection(secretPath, rolePath)
+		secrets, err := om.vaultClient.GetDynamicCredentials(om.ctx, rolePath)
 		if err != nil {
 			return nil, fmt.Errorf("vault read :%w", err)
 		}
-		opts := []backup.PostgresOption{
-			backup.WithPostgresCredentials(connection.Username, connection.Password),
-			backup.WithPostgresDatabase(connection.Database),
-			backup.WithPostgresHost(connection.Host),
-			backup.WithPostgresPort(connection.Port),
-			backup.WithPostgresMethod(instance.Method),
+		opts := []database.PostgresOption{
+			database.WithPostgresCredentials(secrets.Username, secrets.Password),
+			database.WithPostgresDatabase(instance.Database),
+			database.WithPostgresHost(instance.Host),
+			database.WithPostgresPort(instance.Port),
+			database.WithPostgresMethod(instance.Method),
 		}
-		pg, err := backup.NewPostgres(om.cfg, opts...)
+		pg, err := database.NewPostgres(om.cfg, opts...)
 		if err != nil {
 			return nil, fmt.Errorf(
 				"failed creating Postgres instance %q: %w",
-				connection.Database,
+				instance.Database,
 				err,
 			)
 		}
@@ -102,7 +111,7 @@ func (om *OperationManager) InitializePostgresInstance() ([]backup.Database, err
 }
 
 // InitializeDatabases loads, parses, and validates the YAML config at configPath.
-func (om *OperationManager) InitializeDatabases() ([]backup.Database, error) {
+func (om *OperationManager) InitializeDatabases() ([]database.Database, error) {
 	pg_dbs, err := om.InitializePostgresInstance()
 	if err != nil {
 		return nil, fmt.Errorf("initialize postgres instance: %w", err)
@@ -112,20 +121,23 @@ func (om *OperationManager) InitializeDatabases() ([]backup.Database, error) {
 		return nil, fmt.Errorf("initialize mongodb instance: %w", err)
 	}
 
-	dbs := make([]backup.Database, 0, len(pg_dbs)+len(mg_dbs))
+	dbs := make([]database.Database, 0, len(pg_dbs)+len(mg_dbs))
 	dbs = append(dbs, pg_dbs...)
 	dbs = append(dbs, mg_dbs...)
 
 	return dbs, nil
 }
 
+// FIX: write metadata logic is inconsistant, I need to clean it
+// I don't think that using `backupPath` is good here
+
 // BackupDatabase runs a single backup against one Database.
 // It returns an error if that backup fails.
 func (om *OperationManager) BackupDatabase(
-	db backup.Database,
+	db database.Database,
 ) error {
 	start := time.Now()
-	record := backup.Metadata{
+	record := database.Metadata{
 		Database:  db.GetName(),
 		Engine:    db.GetEngine(),
 		StartedAt: start,
@@ -165,9 +177,7 @@ func (om *OperationManager) BackupDatabase(
 // It returns an error if that restore fails.
 // NOTE: Check for metadata.json in the backup directory,
 // NOTE: if not exist, return an error
-// FIX: I should remove the RestoreDatabase() and the BackupDatabase() methods form
-// operation manager
-func (om *OperationManager) RestoreDatabase(db backup.Database, record backup.Metadata) error {
+func (om *OperationManager) RestoreDatabase(db database.Database, record database.Metadata) error {
 	// decompress the file if the compress=true
 	if om.cfg.Backup.Compress {
 		decPath, err := DecompressZstd(record.FilePath)
@@ -177,7 +187,7 @@ func (om *OperationManager) RestoreDatabase(db backup.Database, record backup.Me
 		record.FilePath = decPath
 
 		// Remove the decompressed files
-		defer backup.RemoveFile(record.FilePath)
+		defer database.RemoveFile(record.FilePath)
 	}
 	if err := db.Restore(record.FilePath); err != nil {
 		return fmt.Errorf("restore failed: %w", err)
@@ -207,7 +217,7 @@ func BackupAll(configPath string) error {
 
 		wg.Add(1)
 		// start of the goroutine
-		go func(db backup.Database) {
+		go func(db database.Database) {
 			// mark this goroutine  as DONE (finished) once this function finish(exit)
 			defer wg.Done()
 
@@ -247,13 +257,13 @@ func RestoreAll(configPath string) error {
 		return fmt.Errorf("initialize databases: %w", err)
 	}
 
-	record := backup.Metadata{}
+	record := database.Metadata{}
 	var wg sync.WaitGroup
 
 	for _, db := range databases {
 		wg.Add(1)
 
-		go func(db backup.Database, record backup.Metadata) {
+		go func(db database.Database, record database.Metadata) {
 			defer wg.Done()
 			// increament my waiting list by one since I'm doing a new backup
 			metadataFile := filepath.Join(
